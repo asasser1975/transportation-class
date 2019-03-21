96a8baa5aa4d790e404e9b39ace909e6b0dd1cd6
Alex Sasser and Rohan Straker <alex@>
2019-03-20 18:40:55 -0700

    added fixes to the brakes

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,23 @@
-$(document).ready(function() {
-  $("form#transportation_survey").submit.(function(event) {
+// $(document).ready(function() {
+//   $("form#transportation_survey").submit(function(event) {
+//     event.preventDefault();
+//     $("#work-responses").show();
+//     $("input:checkbox[name=work-transportation]:checked").each(function() {
+//       var workTransportationMode = $(this).val();
+//       $('#work-responses').append(workTransportationMode + "<br>");
+//     });
+//     $('transportation_survey').hide();
+//   });
+// });
+
+$(document).ready(function(){
+  $("form#transportation_survey").submit(function(event){
     event.preventDefault();
     $("#work-responses").show();
-    $("input:checkbox[name=work-transportation]:checked").each(function() {
+    $("input:checkbox[name=work-transportation]:checked").each(function(){
       var workTransportationMode = $(this).val();
       $('#work-responses').append(workTransportationMode + "<br>");
     });
-    $('transportation_survey').hide();
+    $('#transportation_survey').hide();
   });
 });
